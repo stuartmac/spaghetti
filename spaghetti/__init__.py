46a8ebaa7a1a5fcf26ba4967a11446a5f2c21b43
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2019-08-16 22:30:18 +0100

    Add Spaghetti class with placeholder methods

--- spaghetti/__init__.py
+++ spaghetti/__init__.py
@@ -6,3 +6,5 @@ Spaghetti! Disentangle a web of file cross references.
 __author__ = "Stuart MacGowan"
 __license__ = "MIT"
 
+
+from .spaghetti import Spaghetti
--- spaghetti/spaghetti.py
+++ spaghetti/spaghetti.py
@@ -1,5 +1,28 @@
+import networkx as nx
+
+
 # TODO: Extract all valid paths (check exists) from a file
 # TODO: Extract all possible paths from a file and markup with validity
 # TODO: Represent a file's cross references as a graph
 # TODO: Iterate over discovered files
 # TODO: Build a graph for a set of files
+
+
+class Spaghetti:
+    def __init__(self, paths):
+        self.paths = paths
+        self.filerefs = {k: [] for k in paths}
+        self.graph = nx.DiGraph()
+        self.find_file_references()
+        self.build_graph()
+
+    def find_file_references(self):
+        for path in self.paths:
+            with open(path, 'r') as file:
+                # will update to self.filerefs
+                pass
+
+    def build_graph(self):
+        for path, crossrefs in self.filerefs.items():
+            # Will update self.graph
+            pass
