67c767a280b0716d8f47ed58d19fe778056fbbba
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2019-08-16 18:38:52 +0100

    Add some TODOs

---
+++ spaghetti/spaghetti.py
@@ -0,0 +1,5 @@
+# TODO: Extract all valid paths (check exists) from a file
+# TODO: Extract all possible paths from a file and markup with validity
+# TODO: Represent a file's cross references as a graph
+# TODO: Iterate over discovered files
+# TODO: Build a graph for a set of files
