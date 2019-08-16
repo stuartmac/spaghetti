cddbc4ae4c408cedc4caaa3215311a4096e6818a
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2019-08-16 18:42:54 +0100

    Add test placeholder for path regex

---
+++ spaghetti/tests/test_path_regex.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class PathDetection(unittest.TestCase):
+    def test_simple_path_detection(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
