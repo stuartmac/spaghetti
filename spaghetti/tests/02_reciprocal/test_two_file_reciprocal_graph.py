5f71cbc481c1d45bd3eaaf73c7a09b80353e0cd2
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2019-08-16 17:53:18 +0100

    Add first test

---
+++ spaghetti/tests/02_reciprocal/test_two_file_reciprocal_graph.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TwoFileGraphTestCase(unittest.TestCase):
+    def test_two_file_graph(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
